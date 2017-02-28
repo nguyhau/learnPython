22d02b6dc20c900c018dcd8ebecc96a39a40ee9f
nguyhau <nguyhautt1@gmail.com>
2017-02-28 16:25:51 +0700

    create model

--- src/mysite/mysite/urls.py
+++ src/mysite/mysite/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
-    url(r'^login/', 'loginView'),
-    url(r'^greetin/', 'formView'),
-    url(r'^logout/', 'logoutView'),
+#urlpatterns = patterns( 'login.views',
+#                        url(r'^login/', 'loginView'),
+#                        url(r'^greeting/', 'formView'),
+#                        url(r'^logout/', 'logoutView')
+#            )
 ]
+
