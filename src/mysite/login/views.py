92e2ba947d4a43bd7dffaf02aef1857e74c68b03
nguyhau <nguyhautt1@gmail.com>
2017-02-28 07:18:38 +0700

    create login view

---
+++ src/mysite/login/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+# Create your views here.
+from django.http import HttpResponse
+from login.forms import LoginForm
+def loginView(request):
+	username = "Wrong username or password"
+	if request.method == "POST":
+	   MyLoginForm = LoginForm(request.POST)
+	   if MyLoginForm.is_valid():
+		if MyLoginForm.cleaned_data['username'] == 'admin':
+		  if MyLoginForm.cleaned_data['password'] == '123':
+			username = MyLoginForm.cleaned_data['username']
+			request.session['username'] = username
+			request.session.set_expiry(15);
+	else:
+	   MyLoginForm = LoginForm()
+	return render(request, 'loggedin.html', {'username':username})
+def formView(request):
+	if request.session.has_key('username'):
+		username = request.session['username']
+		return render(request, 'loggedin.html', {'username':username})
+	else:
+		return render(reuqest, 'login.html', {})
+def logoutView(request):
+	try: 
+		del request.session['username']
+	except:
+		pass
+	return HttpResponse("Good bye!")
--- src/mysite/mysite/urls.py
+++ src/mysite/mysite/urls.py
@@ -19,4 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
+    url(r'^login/', 'loginView'),
+    url(r'^greetin/', 'formView'),
+    url(r'^logout/', 'logoutView'),
 ]
