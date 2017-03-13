a1c83d124264a7211cf42f36b7bffc9c1524e504
nguyhau <nguyhautt1@gmail.com>
2017-03-13 23:24:17 +0700

    using template

--- src/newsite/polls/models.py
+++ src/newsite/polls/models.py
@@ -1,5 +1,5 @@
 # from django.db import models
-
+# 2017/03/13 22:01:58 socat[25] E exactly 2 addresses required (there are 0); use option "-h" for help                    ssh_exchange_identification: Connection closed by remote host
 # Create your models here.
 from django.db import models
 class Question(models.Model):
--- src/newsite/polls/views.py
+++ src/newsite/polls/views.py
@@ -13,13 +13,38 @@ def detail(request, question_id):
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+from django.shortcuts import get_object_or_404, render
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
+from .crawl import termBrief
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    selected_choice = request.POST['FirstName']
+    para = termBrief(selected_choice)
+    template = loader.get_template('polls/results.html')
+    context = {
+        'content': para,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse(para)
+    # question = get_object_or_404(Question, pk=question_id)
+    # try:
+    #     # selected_choice = question.choice_set_get(pk=request.POST['choice'])
+    #
+    # except (KeyError, Choice.DoesNotExist):
+    #     return render(request, 'polls/detail.html', {
+    #         'question': question,
+    #         'error_message': "You didn't select a choice.",
+    #     })
+    # else:
+    #     selected_choice.votes += 1
+    #     selected_choice.save()
+    # return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     output = ', '.join([q.question_text for q in latest_question_list])
@@ -27,7 +52,7 @@ def vote(request, question_id):
 def index(request):
     try:
         from django.utils import timezone
-        q = Question(question_text="What's new?", pub_date=timezone.now())
+        q = Question(question_text="What's search?", pub_date=timezone.now())
         q.save()
         latest_question_list = Question.objects.order_by('-pub_date')[:1]
         template = loader.get_template('polls/index.html')
